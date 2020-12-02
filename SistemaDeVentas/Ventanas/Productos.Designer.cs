c40c4868fa804654f20ce3a9ecc607907ba241ab
OscarBaeza <71519254+OscarBaeza@users.noreply.github.com>
2020-12-02 13:17:43 -0600

    Se comenzo a crear la interfaz de productos

---
+++ SistemaDeVentas/Ventanas/Productos.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace SistemaDeVentas.Ventanas
+{
+    partial class Productos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtgvProductos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvProductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtgvProductos
+            // 
+            this.dtgvProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgvProductos.Location = new System.Drawing.Point(67, 12);
+            this.dtgvProductos.Name = "dtgvProductos";
+            this.dtgvProductos.RowHeadersWidth = 62;
+            this.dtgvProductos.RowTemplate.Height = 28;
+            this.dtgvProductos.Size = new System.Drawing.Size(844, 303);
+            this.dtgvProductos.TabIndex = 0;
+            // 
+            // Productos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1002, 584);
+            this.Controls.Add(this.dtgvProductos);
+            this.Name = "Productos";
+            this.Text = "Productos";
+            this.Load += new System.EventHandler(this.Productos_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvProductos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtgvProductos;
+    }
+}
