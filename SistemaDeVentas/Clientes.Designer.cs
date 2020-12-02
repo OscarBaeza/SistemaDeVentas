64584c3311a2bdddef5dd9eb7ddb3f596833edbc
carlos-wons <72568840+carlos-wons@users.noreply.github.com>
2020-12-02 16:22:03 -0600

    commit CRUD clientes

---
+++ SistemaDeVentas/Clientes.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace SistemaDeVentas
+{
+    partial class Clientes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtgvClientes = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvClientes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtgvClientes
+            // 
+            this.dtgvClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgvClientes.Location = new System.Drawing.Point(124, 74);
+            this.dtgvClientes.Name = "dtgvClientes";
+            this.dtgvClientes.Size = new System.Drawing.Size(491, 199);
+            this.dtgvClientes.TabIndex = 0;
+            this.dtgvClientes.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgvClientes_CellContentClick);
+            // 
+            // Clientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dtgvClientes);
+            this.Name = "Clientes";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvClientes)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtgvClientes;
+    }
+}
---
+++ SistemaDeVentas/Clientes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaDeVentas
+{
+    public partial class Clientes : Form
+    {
+        public Clientes()
+        {
+            InitializeComponent();
+        }
+
+        private void Clientes_Load(object sender, EventArgs e)
+        {
+            dtgvClientes.DataSource = Funciones.mostrarCliente();
+        }
+
+        private void dtgvClientes_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
